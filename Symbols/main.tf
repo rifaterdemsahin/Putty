045e83c38019439768de77a25006abf4a9592899
Erdem <rifaterdemsahin@gmail.com>
2024-10-31 15:11:22 +0000

    Rename AWS instance resource and update tags for clarity

--- Symbols/main.tf
+++ Symbols/main.tf
@@ -1,8 +1,8 @@
-    resource "aws_instance" "example" {
+    resource "aws_instance" "poc" {
       ami           = "ami-0c55b159cbfafe1f0" # Update with your preferred AMI
       instance_type = "t2.micro"
 
       tags = {
-        Name = "example-instance"
+        Name = "poc-instance"
       }
     }
